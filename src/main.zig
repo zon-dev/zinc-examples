e998e9c4021a3be2c754e691de40b6d1c3e9badf
dravenk <longxianwen@outlook.com>
2024-07-17 11:06:41 +0800

    update example

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
+const std = @import("std");
 const zinc = @import("zinc");
 pub fn main() !void {
-    const engine = zinc.Engine.default();
+    const engine = try zinc.Engine.default();
+    std.debug.print("Listening on: 127.0.0.1:{any}\n", .{ engine.get_port() });
     _ = try engine.run();
 }
