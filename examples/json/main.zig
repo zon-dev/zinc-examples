53efb35e1ea609cf5b3dcce8b832ab64c8e131e1
dravenk <longxianwen@outlook.com>
2024-08-01 15:42:36 +0800

    Update samples

--- examples/json/main.zig
+++ examples/json/main.zig
@@ -1,16 +1,14 @@
-const std = @import("std");
-const http = std.http;
 const z = @import("zinc");
 
 pub fn main() !void {
     var engine = try z.Engine.new(.{ .port = 8080 });
 
     var router = &engine.router;
-    try router.get("/", jsonString);
+    try router.get("/", hello_world);
 
     _ = try engine.run();
 }
 
-fn jsonString(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+fn hello_world(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
     try ctx.JSON(.{}, .{ .message = "Hello, World!" });
 }
