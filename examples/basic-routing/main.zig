b81a282fb1ea46d073ba36cc522955ee3488342a
dravenk <longxianwen@outlook.com>
2024-08-01 12:39:13 +0800

    Update basic-routing sample

--- examples/basic-routing/main.zig
+++ examples/basic-routing/main.zig
@@ -1,9 +1,7 @@
-const std = @import("std");
 const z = @import("zinc");
 
 pub fn main() !void {
     var engine = try z.Engine.new(8080);
-    std.debug.print("Listening on: 127.0.0.1:{any}\n", .{engine.getPort()});
 
     var router = &engine.router;
     try router.get("/hello", hello);
@@ -12,6 +10,5 @@ pub fn main() !void {
 }
 
 fn hello(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
-    std.debug.print("Hello!\n", .{});
     try res.send("Hello!");
 }
