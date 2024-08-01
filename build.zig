460708e88fd2fbdd69fcc7b710248372a05975c4
dravenk <longxianwen@outlook.com>
2024-08-01 12:29:04 +0800

    Adding basic-routing sample

--- build.zig
+++ build.zig
@@ -9,23 +9,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // const exe = b.addExecutable(
-    //     .{
-    //     .name = "basic",
-    //     .root_source_file = b.path( "examples/basic/main.zig" ),
-    //     .target = target,
-    //     .optimize = optimize,
-    // }
-    // );
-    // exe.root_module.addImport("zinc", zinc.module("zinc"));
-    // b.installArtifact(exe);
-    // const run_cmd = b.addRunArtifact(exe);
-
     inline for ([_]struct {
         name: []const u8,
         src: []const u8,
     }{
         .{ .name = "basic", .src = "examples/basic/main.zig" },
+        .{ .name = "basic-routing", .src = "examples/basic-routing/main.zig" },
         .{ .name = "simple", .src = "examples/simple/main.zig" },
     }) |execfg| {
         const exe_name = execfg.name;
