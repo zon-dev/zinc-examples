cb4fb9c8f24844c3f10479fa5fb04d81dac42542
dravenk <longxianwen@outlook.com>
2024-08-03 10:56:50 +0800

    Update samples

--- examples/simple/main.zig
+++ examples/simple/main.zig
@@ -2,33 +2,38 @@ const std = @import("std");
 const z = @import("zinc");
 
 pub fn main() !void {
-    var zinc = try z.Engine.init(.{ .port = 8080 });
+    var zinc = try z.Engine.init(.{
+        .allocator = std.heap.page_allocator,
+        .port = 8080,
+    });
 
     std.debug.print("Listening on: {any}\n", .{zinc.getAddress()});
 
     var router = zinc.getRouter();
     try router.get("/hello", hello);
     try router.post("/hi", hi);
-    try router.add(std.http.Method.GET, "/ping", ping);
+    try router.add(&.{ .GET, .POST }, "/ping", pong);
 
     for (router.getRoutes().items) |r| {
-        std.debug.print("{s} | {s}\n", .{ @tagName(r.http_method), r.path });
+        for (r.methods) |m| {
+            std.debug.print("{s} | {s}\n", .{ @tagName(m), r.path });
+        }
     }
 
-    _ = try zinc.run();
+    try zinc.run();
 }
 
-fn hello(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
+fn hello(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
     std.debug.print("Hello!\n", .{});
-    try res.sendBody("Hello!");
+    try ctx.Text(.{}, "Hello!");
 }
 
-fn hi(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
+fn hi(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
     std.debug.print("hi!\n", .{});
-    try res.sendBody("hi!");
+    try ctx.Text(.{}, "hi!");
 }
 
-fn ping(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
+fn pong(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
     std.debug.print("pong!\n", .{});
-    try res.sendBody("pong!");
+    try ctx.Text(.{}, "pong!");
 }
