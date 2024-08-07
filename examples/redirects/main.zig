ee808d2bda9f9a45e0d64775657b264420f27fe6
dravenk <longxianwen@outlook.com>
2024-08-07 12:02:54 +0800

    Adding redirectToGitHub

--- examples/redirects/main.zig
+++ examples/redirects/main.zig
@@ -5,13 +5,19 @@ pub fn main() !void {
 
     var router = zinc.getRouter();
     try router.get("/test", redirect);
+    try router.get("/github", redirectToGitHub);
     try router.get("/hello", helloWorld);
 
     try zinc.run();
 }
 
 fn redirect(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
-    try ctx.redirect("/hello");
+    // Redirect to /hello, with a 302 status code
+    try ctx.redirect(.found, "/hello");
+}
+fn redirectToGitHub(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    // Redirect to https://github.com, with a 301 status code
+    try ctx.redirect(.moved_permanently,"https://github.com");
 }
 
 fn helloWorld(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
