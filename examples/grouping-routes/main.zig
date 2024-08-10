f7943b1fa19e9fe54ff79d94de359fcebfe849f0
dravenk <longxianwen@outlook.com>
2024-08-10 16:49:30 +0800

    Update examples

--- examples/grouping-routes/main.zig
+++ examples/grouping-routes/main.zig
@@ -5,7 +5,6 @@ pub fn main() !void {
     var z = try zinc.init(.{ .port = 8080 });
 
     var router = z.getRouter();
-    try router.get("/", helloWorld);
 
     // /api
     var group = try router.group("/api", api);
@@ -14,15 +13,11 @@ pub fn main() !void {
     // /api/v2
     try group.get("/v2", v2);
 
-    for (router.routes.items) |route| {
+    for (router.getRoutes().items) |route| {
         std.debug.print("Route: {s}\n", .{route.path});
     }
 
     try z.run();
-}
-
-fn helloWorld(ctx: *zinc.Context, _: *zinc.Request, _: *zinc.Response) anyerror!void {
-    try ctx.Text(.{}, "Hello world!");
 }
 
 fn api(ctx: *zinc.Context, _: *zinc.Request, _: *zinc.Response) anyerror!void {
