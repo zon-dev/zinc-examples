80c5105fa30045ad86a6b355e7145a7011838d3f
dravenk <longxianwen@outlook.com>
2024-08-24 15:47:55 +0800

    update exmaples

--- examples/basic-routing/main.zig
+++ examples/basic-routing/main.zig
@@ -1,14 +1,25 @@
+const std = @import("std");
 const zinc = @import("zinc");
 
 pub fn main() !void {
     var z = try zinc.init(.{ .port = 8080 });
 
     var router = z.getRouter();
-    try router.get("/", helloWorld);
+    try router.get("/hello", helloWorld);
+    try router.post("/hi", hi);
+    try router.add(&.{ .GET, .POST }, "/ping", pong);
 
     try z.run();
 }
 
 fn helloWorld(ctx: *zinc.Context) anyerror!void {
-    try ctx.text("Hello world!", .{});
+    try ctx.json(.{ .message = "Hello, World!" }, .{});
+}
+
+fn hi(ctx: *zinc.Context) anyerror!void {
+    try ctx.html("<h1>Hi!</h1>", .{});
+}
+
+fn pong(ctx: *zinc.Context) anyerror!void {
+    try ctx.text("pong!", .{});
 }
