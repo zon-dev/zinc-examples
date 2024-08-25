cdc3c39b29be25a2b91415e522407ae4a22f5856
dravenk <longxianwen@outlook.com>
2024-08-25 16:58:56 +0800

    update examples

--- examples/query-and-post-form/main.zig
+++ examples/query-and-post-form/main.zig
@@ -12,21 +12,20 @@ pub fn main() !void {
 
 /// POST /post?id=1234&message=hello&message=world HTTP/1.1
 /// Content-Type: application/x-www-form-urlencoded
-/// 
+///
 /// name:jack
 /// friend:mike
-/// 
+///
 fn queryAndForm(ctx: *zinc.Context) anyerror!void {
-    const query_map = ctx.queryMap().?;
-    const ids = query_map.get("id").?;
-    const id = ids.items[0];
-    const messages = query_map.get("message").?;
+    const id = try ctx.queryString("id");
+
+    const messages: std.ArrayList([]const u8) = try ctx.queryValues("message");
 
     const form = ctx.postFormMap().?; // form is a map
     const name = form.get("name").?;
     const friend = form.get("friend").?;
 
-    const bf = try std.fmt.allocPrint(std.heap.page_allocator, "id: {s}\nname: {s} \nfriend: {s}\nmessages: {s} {s}", .{ id, name, friend, messages.items[0],messages.items[1] });
+    const bf = try std.fmt.allocPrint(std.heap.page_allocator, "id: {s}\nname: {s} \nfriend: {s}\nmessages: {s} {s}", .{ id, name, friend, messages.items[0], messages.items[1] });
 
     try ctx.text(bf, .{});
 }
